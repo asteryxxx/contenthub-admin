4ab7b435619d48db325276e9b77bec871cda80f8
asteryxxx <437767931@qq.com>
2021-02-05 16:33:22 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
-
+// 创建Vue根实例
+// 把router配置到根实例中
+// 通过render方法把app根组件渲染到#app入口 节点
 new Vue({
   router,
   render: h => h(App)
+  // el : '#app' 等价 $mount("#app")
 }).$mount('#app')
